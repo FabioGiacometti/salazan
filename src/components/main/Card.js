1c9b5735fb92765e13958f128ad44e18bcd6079c
Fabio Giacometti <fabio.giacometti@ross.com.ar>
2020-03-22 10:06:14 -0300

    working on card body

--- src/components/main/Card.js
+++ src/components/main/Card.js
@@ -1,19 +1,25 @@
 import React from "react";
-import vehiculos from "../../assets/vehiculos";
+import card from './card.scss'
 
+const url = "http://www.salazanautomotores.com.ar/"
 export default class Card extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
+    const detalle = this.props.vehiculo.detalles
+    const verMas = `${url}${detalle}`
+    // console.log("%cesto es detalle", "font-size:30px", this.props.detalles)
     return (
-      <div className="h-auto w-auto ml-4 mt-4 bg-green-500" style={{width: "232px"}}>
-        <img src="https://via.placeholder.com/232x174?text=Imagen+prueba+salazan+232+174" alt=""/>
-        <h2 className="text-sm color-red-500 whitespace-pre-wrap" >{this.props.vehiculo.modelo}</h2>
-        <p>
-            
-        </p>
+      <div className="h-auto w-auto ml-4 mt-4 rounded cardBody">
+        <img className="w-full" src="https://via.placeholder.com/232x174?text=Imagen+prueba+salazan+232+174" alt=""/>
+        <div className="p-4">
+          <h2 className="text-sm color-red-500 whitespace-pre-wrap" >{this.props.vehiculo.modelo}</h2>
+          <p className="text-sm color-red-500 whitespace-pre-wrap" >{this.props.vehiculo.specs}</p>
+          <p className="text-sm color-red-500 whitespace-pre-wrap" >{this.props.vehiculo.precio}</p>
+          <a href={verMas}> Ver Mas</a>
+        </div>
       </div>
     );
   }
