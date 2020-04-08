7a425d2188c4214e5d46ecbbdb6989271ddb9e5e
Fabio Giacometti <fabio.giacometti@ross.com.ar>
2020-04-08 15:44:49 -0300

    styles a gogo

---
+++ build/precache-manifest.a8dc8612deb9d39ccb767cbff9c0aa15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "746c283166f6196494cf61104c1bc397",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38a0b8dfc0cf22a439d5",
+    "url": "/static/css/main.097912cd.chunk.css"
+  },
+  {
+    "revision": "32415b7b92f19fc280cf",
+    "url": "/static/js/2.ece5dc6e.chunk.js"
+  },
+  {
+    "revision": "ddfd09a9397ea336a2550edb6da27f94",
+    "url": "/static/js/2.ece5dc6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38a0b8dfc0cf22a439d5",
+    "url": "/static/js/main.8e9aa4fb.chunk.js"
+  },
+  {
+    "revision": "ef7b2c7893e55fab8ba6",
+    "url": "/static/js/runtime-main.f04f2b37.js"
+  }
+]);
