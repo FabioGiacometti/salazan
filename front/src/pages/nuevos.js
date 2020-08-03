e825cdcc8a20b1b19584a8db38b268129b679bf0
Fabio Giacometti <fabio.giacometti@ross.com.ar>
2020-08-03 18:18:34 -0300

    redireccionado el boton de contacto

--- front/src/pages/nuevos.js
+++ front/src/pages/nuevos.js
@@ -41,7 +41,7 @@ const Nuevos = () => {
           </p>
         </div>
       </div>
-      <iframe width="853" height="480" src="https://www.youtube.com/embed/ryrb1rhLDIY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="video" className="mx-auto"></iframe>
+      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/ryrb1rhLDIY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="video" className="mx-auto"></iframe>
     </div>
   );
 };
