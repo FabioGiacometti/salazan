4ce332cdaf673fbf2ced4fe8de2c581e491ab953
Fabio Giacometti <fabio.giacometti@ross.com.ar>
2020-08-12 17:53:27 -0300

    test commit n4

--- front (copia)/src/assets/navigationItems.js
+++
@@ -1,9 +0,0 @@
-const items = [
-  { icon: "newCar", title: "Nuevos", dir: "/nuevos" },
-  { icon: "usedCar", title: "Usados", dir: "/usados" },
-  { icon: "finaciacion", title: "Finaciación", dir: "/finaciacion" },
-  { icon: "empresa", title: "Empresa", dir: "/empresa" },
-  { icon: "contacto", title: "Contacto", dir: "/contacto" },
-];
-
-export default items;
