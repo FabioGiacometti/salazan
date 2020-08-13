b9b68c6adcede706e980978ec792ebbde46231ef
Fabio Giacometti <fabio.giacometti@ross.com.ar>
2020-08-13 14:48:55 -0300

    agregado index.html al router

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Details from "./pages/details";
@@ -8,28 +8,33 @@ import Nuevos from "./pages/nuevos";
 import Financiacion from "./pages/financiacion";
 import Empresa from "./pages/empresa";
 import Footer from "./components/footer/footer";
-import Usados from "./pages/usados"
-class App extends React.Component {
-  render() {
-    return (
-      <div style={{ fontFamily: "sans-serif" }}>
-        <BrowserRouter>
-          <Header />
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/index" exact component={Home} />
-            <Route path="/usados" exact component={Usados} />
-            <Route path="/:id/detalle" component={Details} />
-            <Route path="/nuevos" component={Nuevos} />
-            <Route path="/finaciacion" component={Financiacion} />
-            <Route path="/empresa" component={Empresa} />
-            <Route path="/contacto" component={Contact} />
-          </Switch>
-          <Footer/>
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
+import Usados from "./pages/usados";
+
+const App = () => {
+  const [busqueda, setBusqueda] = useState();
+
+  return (
+    <div style={{ fontFamily: "sans-serif" }}>
+      <BrowserRouter>
+        <Header getSearch={setBusqueda} />
+        <Switch>
+          <Route
+            path={["/", "index.html", "/home", "/index", "/index.html"]}
+            exact
+            render={() => <Home busqueda={busqueda} />}
+          />
+          <Route path="/usados" render={() => <Usados busqueda={busqueda} />} />
+
+          <Route path="/:id/detalle" component={Details} />
+          <Route path="/nuevos" component={Nuevos} />
+          <Route path="/finaciacion" component={Financiacion} />
+          <Route path="/empresa" component={Empresa} />
+          <Route path="/contacto" component={Contact} />
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
--- front/src/components/header/Searchbar.js
+++ front/src/components/header/Searchbar.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import search from "../../assets/images"
 
-export default class Searchbar extends React.Component{
-
-    render(){
-        return(
-            <div className="flex w-1/3 sm:w-auto bg-gray-100 rounded-full items-center mx-0 mr-4 sm:mx-4 pr-3 shadow-inner relative ">
-                <input type="text" className="w-full pl-4 py-4 h-4 rounded-full bg-gray-100"/>
-                <img src={require("../../assets/images/search.png")} alt="" className="h-3"/>
-            </div>
-        )
-    }
-}
+const Searchbar = (props) => {
+    const [searchValue, setSearchValue] = useState();
+    const [isSearching, setIsSearching] = useState(false)
+    
+    return (
+      <div className="flex w-1/3 sm:w-auto bg-gray-100 rounded-full items-center mx-0 mr-4 sm:mx-4 pr-3 shadow-inner relative focus:outline-none">
+        <input
+          placeholder="Introduzca su busqueda"
+          type="text"
+          // value=""
+          className="w-full pl-4 py-4 h-4 rounded-full bg-gray-100"
+          onChange={(e) => {props.funcion.getSearch(e.target.value); setIsSearching(true)}}
+        />  
+        {!isSearching ? (
+          <img
+            src={require("../../assets/images/search.png")}
+            alt=""
+            className="h-4 cursor-pointer absolute right-0 mr-2"
+            onClick={() => props.funcion.getSearch(searchValue)}
+          />
+        ): null}
+      </div>
+    );
+  };
+  
+  export default Searchbar;
